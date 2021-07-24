4dc5f7649bd0d7b3b095cec211353cbd9b0d8ffe
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-07-24 19:37:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function preload() {
 }
-
 function setup() {
-    canvas=createCanvas(600,400);
-    canvas.center();
+  canvas = createCanvas(600, 500);
+  canvas.position(650, 370);
+  video = createCapture(VIDEO);
+  video.hide();
+
 }
+function draw() {
+
+}
+
+
